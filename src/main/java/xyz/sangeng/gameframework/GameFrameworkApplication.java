fb818d32dd8d18c2b0398c19902ed418c77acf64
huanghailiang <hailianghuang@outlook.com>
2022-09-09 20:35:15 +0800

    初始化项目

---
+++ src/main/java/xyz/sangeng/gameframework/GameFrameworkApplication.java
@@ -0,0 +1,13 @@
+package xyz.sangeng.gameframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/sangeng/gameframework/GameFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sangeng.gameframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
